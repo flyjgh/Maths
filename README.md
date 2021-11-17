142c1ec6916b788f76cfdb786e8129cee0df2f79
flyjgh <doctorzwei@gmail.com>
2021-11-17 22:01:38 +0100

    nw

---
+++ README.md
@@ -0,0 +1,5 @@
+# Formalism
+
+## really not formal, but fun
+
+Having fun with theorem proving, proof checking, and the Curry-Howard correspondence in general.
--- category/optics.jl
+++ category/optics.jl
@@ -10,14 +10,12 @@ OpticRepr(o::AbstractOptic) =
 left(o::OpticRepr, a) = o.l(a)
 right(o::OpticRepr, m, b′) = o.r(m, b′)
 
-# Block style optics interface
 function (o::AbstractOptic)(f::Function, a)
     m, b = left(o, a)
     b′ = f(b)
     right(o, m, b′)
 end
 
-# Continuation style optics interface
 function (o::AbstractOptic)(a)
     m, b = left(o, a)
     b, b′->right(o, m, b′)
